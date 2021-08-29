b6288e6e32025cebf3dd4822a001bc0d47983313
JJ Merelo <jjmerelo@gmail.com>
2021-08-29 19:54:37 +0200

    Elimina chalk

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
-const chalk = require('chalk')
 import get_diff from "./grading.js"
 
 try {
@@ -13,22 +12,19 @@ try {
         core.setFailed( "🍐🔥❌ Debes cambiar exactamente 1 fichero, hay ❌" + diff.length + "❌ en el pull request" )
     }
     const file = diff[0]
-    console.log( "✅ Hay solo un fichero 📁 " + chalk.inverse(file.from) + " en el pull request")
+    console.log( "✅ Hay solo un fichero 📁 " + file.from + "📁 en el pull request")
 
     if ( file.additions != 1 ) {
 	core.setFailed( "🍐🔥❌ Debes cambiar exactamente 1 línea en el fichero, hay ❌" + file.additions + "❌ cambiadas en el pull request" )
     }
     core.info( "✅ Hay solo una línea cambiada en el pull request")
 
-    const line = file.chunks[0].changes[0]
-    console.log(line)
-    console.log(line['content'])
-    console.log(line.content)
+    const line = file.chunks[0].changes[0].content
     if (  line.indexOf( "github.com" ) < 0 ) {
 	core.setFailed( "🍐🔥❌ El cambio debe incluir el URL de la rama " )
     }
 
-    var ghRepoMatch = /github.com\/(\S+)\/(.+?)(:\s+|\))/.exec(content)
+    var ghRepoMatch = /github.com\/(\S+)\/(.+?)(:\s+|\))/.exec(line)
     console.log(ghRepoMatch)
 } catch (error) {
     core.setFailed("❌ Algo indeterminado ha fallado ❌. Mira el mensaje: " + error.message);
