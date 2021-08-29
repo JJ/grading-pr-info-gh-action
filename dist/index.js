c0da4a98cbff33fe806f04aea0875142fcca711f
JJ Merelo <jjmerelo@gmail.com>
2021-08-29 18:38:08 +0200

    Añade mensaje con 1 línea :white_check_mark:

--- dist/index.js
+++ dist/index.js
@@ -6208,17 +6208,20 @@ try {
     const token = process.env.GITHUB_TOKEN
     const octokit = new github.getOctokit(token)
     const diff = await (0,_grading_js__WEBPACK_IMPORTED_MODULE_0__/* .default */ .Z)( context, octokit )
+
     if ( diff.length != 1 ) {
         core.setFailed( "🍐🔥❌ Debes cambiar exactamente 1 fichero, hay ❌" + diff.length + "❌ en el pull request" );
     }
     const file = diff[0];
     core.info( "✅ Hay solo un fichero en el pull request")
-    core.debug(file);
+    core.info(file);
+
     if ( file.additions != 1 ) {
 	core.setFailed( "🍐🔥❌ Debes cambiar exactamente 1 línea en el fichero, hay ❌" + file.additions + "❌ cambiadas en el pull request" );
     }
+    core.info( "✅ Hay solo una sola línea cambiada en el pull request")
 } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed("❌ Algo indeterminado ha fallado ❌. Mira el mensaje: " + error.message);
 }
 
 __webpack_handle_async_dependencies__();
--- src/index.js
+++ src/index.js
@@ -7,15 +7,18 @@ try {
     const token = process.env.GITHUB_TOKEN
     const octokit = new github.getOctokit(token)
     const diff = await get_diff( context, octokit )
+
     if ( diff.length != 1 ) {
         core.setFailed( "🍐🔥❌ Debes cambiar exactamente 1 fichero, hay ❌" + diff.length + "❌ en el pull request" );
     }
     const file = diff[0];
     core.info( "✅ Hay solo un fichero en el pull request")
-    core.debug(file);
+    core.info(file);
+
     if ( file.additions != 1 ) {
 	core.setFailed( "🍐🔥❌ Debes cambiar exactamente 1 línea en el fichero, hay ❌" + file.additions + "❌ cambiadas en el pull request" );
     }
+    core.info( "✅ Hay solo una sola línea cambiada en el pull request")
 } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed("❌ Algo indeterminado ha fallado ❌. Mira el mensaje: " + error.message);
 }
