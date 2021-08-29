956e9dbdce59e53294327a40536000807555369d
JJ Merelo <jjmerelo@gmail.com>
2021-08-29 13:21:11 +0200

    Añade ficheros y tests iniciales

--- src/index.js
+++ src/index.js
@@ -1,23 +1,15 @@
 const core = require('@actions/core');
 const {GitHub, context} = require('@actions/github')
+import "./grading.js"
 
 try {
     const token = core.getInput('github-token', {required: true})
     const github = new GitHub(token, {} )
+
+    const diff = await get_diff( context, github)
+     if ( diff.length != 1 ) {
+        core.setFailed( "🍐🔥❌ Debes cambiar exactamente 1 fichero, hay ❌" + diff.length + "❌ en el pull request" );
+     }
 } catch (error) {
     core.setFailed(error.message);
-}
-
-function get_and_check_file_changed( context, github, core ) {
-    const result = get_diff( context, github )
-    const files = parse(result.data)
-    if ( files.length != 1 ) {
-        core.setFailed( "🍐🔥❌ Debes cambiar exactamente 1 fichero, hay ❌" + files.length + "❌ en el pull request" );
-    }
-
-}
-
-function get_diff( context, github ) {
-    const diff_url = context.payload.pull_request.diff_url
-    return await github.request( diff_url )
 }
